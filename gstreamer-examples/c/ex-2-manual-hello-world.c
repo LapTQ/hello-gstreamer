14911a038ec3c8c355f16f24a842c88b890e0571
LapTQ <lap.tq4@gmail.com>
2024-11-30 11:11:06 +0700

    msg

--- gstreamer-examples/c/ex-2-manual-hello-world.c
+++ gstreamer-examples/c/ex-2-manual-hello-world.c
@@ -1,11 +1,22 @@
 #include <gst/gst.h>
 
-#ifdef __APPLE__
-#include <TargetConditionals.h>
-#endif
 
-int
-tutorial_main (int argc, char *argv[])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ======================================================================
+int main (int argc, char *argv[])
 {
   GstElement *pipeline, *source, *sink;
   GstBus *bus;
@@ -15,28 +26,56 @@ tutorial_main (int argc, char *argv[])
   /* Initialize GStreamer */
   gst_init (&argc, &argv);
 
+
   /* Create the elements */
   source = gst_element_factory_make ("videotestsrc", "source");
   sink = gst_element_factory_make ("autovideosink", "sink");
 
+
+
+
+
+
+
   /* Create the empty pipeline */
   pipeline = gst_pipeline_new ("test-pipeline");
+
+
+
+
+
 
   if (!pipeline || !source || !sink) {
     g_printerr ("Not all elements could be created.\n");
     return -1;
   }
 
+
   /* Build the pipeline */
-  gst_bin_add_many (GST_BIN (pipeline), source, sink, NULL);
+  gst_bin_add_many (
+    GST_BIN (pipeline), 
+    source, 
+    sink, 
+    NULL
+  );
+
+
   if (gst_element_link (source, sink) != TRUE) {
     g_printerr ("Elements could not be linked.\n");
     gst_object_unref (pipeline);
     return -1;
   }
 
+
   /* Modify the source's properties */
   g_object_set (source, "pattern", 0, NULL);
+
+
+
+
+
+
+
 
   /* Start playing */
   ret = gst_element_set_state (pipeline, GST_STATE_PLAYING);
@@ -46,11 +85,15 @@ tutorial_main (int argc, char *argv[])
     return -1;
   }
 
+
   /* Wait until error or EOS */
   bus = gst_element_get_bus (pipeline);
-  msg =
-      gst_bus_timed_pop_filtered (bus, GST_CLOCK_TIME_NONE,
-      GST_MESSAGE_ERROR | GST_MESSAGE_EOS);
+  msg = gst_bus_timed_pop_filtered (
+    bus, 
+    GST_CLOCK_TIME_NONE,
+    GST_MESSAGE_ERROR | GST_MESSAGE_EOS
+  );
+
 
   /* Parse message */
   if (msg != NULL) {
@@ -60,10 +103,8 @@ tutorial_main (int argc, char *argv[])
     switch (GST_MESSAGE_TYPE (msg)) {
       case GST_MESSAGE_ERROR:
         gst_message_parse_error (msg, &err, &debug_info);
-        g_printerr ("Error received from element %s: %s\n",
-            GST_OBJECT_NAME (msg->src), err->message);
-        g_printerr ("Debugging information: %s\n",
-            debug_info ? debug_info : "none");
+        g_printerr ("Error received from element %s: %s\n", GST_OBJECT_NAME (msg->src), err->message);
+        g_printerr ("Debugging information: %s\n", debug_info ? debug_info : "none");
         g_clear_error (&err);
         g_free (debug_info);
         break;
@@ -78,19 +119,10 @@ tutorial_main (int argc, char *argv[])
     gst_message_unref (msg);
   }
 
+
   /* Free resources */
   gst_object_unref (bus);
   gst_element_set_state (pipeline, GST_STATE_NULL);
   gst_object_unref (pipeline);
   return 0;
-}
-
-int
-main (int argc, char *argv[])
-{
-#if defined(__APPLE__) && TARGET_OS_MAC && !TARGET_OS_IPHONE
-  return gst_macos_main ((GstMainFunc) tutorial_main, argc, argv, NULL);
-#else
-  return tutorial_main (argc, argv);
-#endif
 }
--- gstreamer-examples/python/ex-2-manual-hello-video.py
+++ gstreamer-examples/python/ex-2-manual-hello-video.py
@@ -14,8 +14,18 @@ from gi.repository import Gst, GLib, GObject
 logging.basicConfig(level=logging.DEBUG, format="[%(name)s] [%(levelname)8s] - %(message)s")
 logger = logging.getLogger(__name__)
 
+
+# ======================================================================
+
+
+
+
+
+
+
 # Initialize GStreamer
 Gst.init(sys.argv[1:])
+
 
 # Create the elements
 source = Gst.ElementFactory.make("videotestsrc", "source")
@@ -34,18 +44,28 @@ pipeline = Gst.Pipeline.new("test-pipeline")
 # A pipeline is a particular type of Gst.Bin, which is the element used to contain other elements. 
 # Therefore all methods which apply to bins also apply to pipelines.
 
+
 if not pipeline or not source or not sink:
     logger.error("Not all elements could be created.")
     sys.exit(1)
 
 
+
 # Build the pipeline
 pipeline.add(source)
 pipeline.add(sink)
-# we call Gst.Bin.add() to add elements to the pipeline. 
+# we call Gst.Bin.add() to add elements to the pipeline.
+
+
+
+
+
 if not source.link(sink):   # These elements, however, are not linked with each other yet. For this, we need to use Gst.Element.link().
     logger.error("Elements could not be linked.")
     sys.exit(1)
+
+
+
 
 # Modify the source's properties
 source.props.pattern = 0
@@ -56,25 +76,41 @@ source.props.pattern = 0
 # The current state of a property can be fetched by either: `source.props.pattern` or `source.get_property("pattern")`
 # Try changing the value of the pattern property!!!
 
+
 # Start playing
 ret = pipeline.set_state(Gst.State.PLAYING)
 if ret == Gst.StateChangeReturn.FAILURE:
     logger.error("Unable to set the pipeline to the playing state.")
     sys.exit(1)
 
+
+
+
 # Wait for EOS or error
 bus = pipeline.get_bus()
-msg = bus.timed_pop_filtered(Gst.CLOCK_TIME_NONE, Gst.MessageType.ERROR | Gst.MessageType.EOS)
+msg = bus.timed_pop_filtered(
+    Gst.CLOCK_TIME_NONE, 
+    Gst.MessageType.ERROR | Gst.MessageType.EOS
+)
 # Gst.Bus.timed_pop_filtered() waits for execution to end and returns with a Gst.Message.
+
 
 # Parse message
 if msg:
+    
+    
+    
+    
     if msg.type == Gst.MessageType.ERROR:
         err, debug_info = msg.parse_error() # returns a GLib GLib.Error error structure and a string
         logger.error(f"Error received from element {msg.src.get_name()}: {err.message}")
         logger.error(f"Debugging information: {debug_info if debug_info else 'none'}")
+    
+    
+    
     elif msg.type == Gst.MessageType.EOS:
         logger.info("End-Of-Stream reached.")
+    
     else:
         # This should not happen as we only asked for ERRORs and EOS
         logger.error("Unexpected message received.")
@@ -83,5 +119,7 @@ if msg:
 # It is the object responsible for delivering to the application the Gst.Messages generated 
 # by the elements, in order and to the application thread. This last point is important, 
 # because the actual streaming of media is done in another thread than the application.
+
+
 
 pipeline.set_state(Gst.State.NULL)
