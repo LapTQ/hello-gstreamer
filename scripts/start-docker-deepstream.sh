08ba8ce3bc97bc03a067321f01a0c381eae38373
LapTQ <lap.tq4@gmail.com>
2025-08-05 06:26:14 +0700

    .

--- scripts/start-docker-deepstream.sh
+++ scripts/start-docker-deepstream.sh
@@ -1,17 +1,24 @@
+# docker pull nvcr.io/nvidia/deepstream:7.1-gc-triton-devel
+
+xhost +
 docker run \
     -it \
     -d \
+    --gpus all \
+    --runtime nvidia \
+    --network=host \
+    --privileged \
     -v /media:/media \
+    -v /home:/home \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
     -v ${HOME}/.Xauthority:/root/.Xauthority \
     -e DISPLAY=$DISPLAY \
     -e QT_X11_NO_MITSHM=1 \
     -e NVIDIA_DRIVER_CAPABILITIES=all \
     -e NVIDIA_VISIBLE_DEVICES=all \
-    -e DIR_NVDS=/opt/nvidia/deepstream/deepstream \
-    --runtime nvidia \
+    -e PATHD_NVDS=/opt/nvidia/deepstream/deepstream-7.1 \
     --workdir $( pwd ) \
     --name hello-deepstream \
-    nvcr.io/nvidia/deepstream:7.1-triton-multiarch
+    nvcr.io/nvidia/deepstream:7.1-gc-triton-devel
 
 
