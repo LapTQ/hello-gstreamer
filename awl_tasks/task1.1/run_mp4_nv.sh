febec12264432d39e2e4d60a33c4a25e627196c5
LapTQ <lap.tq4@gmail.com>
2026-09-08 16:36:36 +0000

    .

--- awl_tasks/task1.1/run_mp4_nv.sh
+++ awl_tasks/task1.1/run_mp4_nv.sh
@@ -15,3 +15,14 @@ gst-launch-1.0 filesrc location=$IN_FILE ! qtdemux ! h264parse ! nvv4l2decoder !
 # 7. h264parse: để lưu thành file, luồng H.264 phải được cấu trúc lại phù hợp với file MP4.
 # 8. mp4mux: tạo bảng chỉ mục (moov atom), đóng gói thành cấu trúc file MP4,... xuất ra luồng byte 
 # 9. filesink: ghi luồng byte vào file.
+
+# `nvv4l2h264enc -> h264parse -> mp4mux` và `x264enc -> mp4mux`
+# Tại sao x264enc không cần h264parse như nvv4l2h264enc?
+# Câu trả lời nằm ở khả năng hỗ trợ định dạng đầu ra của 2 bộ nén (Encoder) này.
+# - chuẩn nén H.264 có 2 "phong cách" sắp xếp dữ liệu (gọi là stream-format):
+#   + byte-stream: Sắp xếp dữ liệu thành một chuỗi liên tục, các khung hình được ngăn cách bằng một đoạn mã vạch (start codes, ví dụ 00 00 00 01). Kiểu này rất thô, thích hợp để bắn qua mạng (RTSP) hoặc đẩy thẳng vào chip phần cứng.
+#   + avc: Gọn gàng hơn. Thông tin mô tả môi trường (SPS/PPS) được gom lại để lên đầu, các khung hình được gắn "thẻ độ dài" ở trước
+# - `mp4mux` chỉ nhận H.264 dạng avc.
+# - `x264enc` có thể xuất ra cả 2 định dạng byte-stream hoặc avc.
+# - `nvv4l2h264enc` chỉ xuất ra duy nhất luồng H.264 thô ở dạng byte-stream.
+# => h264parse cần đứng ở giữa nvv4l2h264enc để chuyển luồng dạng byte-stream sang dạng avc.
--- awl_tasks/task1.1/run_rtsp_cpu.sh
+++ awl_tasks/task1.1/run_rtsp_cpu.sh
@@ -14,3 +14,9 @@ gst-launch-1.0 -e rtspsrc location=$IN_RTSP ! decodebin ! videoconvert ! x264enc
 
 # Bỏ qua bước decode/encode
 gst-launch-1.0 -e rtspsrc location=$IN_RTSP ! rtph264depay ! h264parse ! mp4mux ! filesink location=$OUT_DIR/output_rtsp.mp4
+
+# Quy trình:
+# (đọc task1.1/run_mp4_nv.sh trước)
+# rtspsrc: 1 hình ảnh H.264 truyền qua mạng bị băm nhỏ và nhồi vào hàng vạn gói tin RTP để truyền qua mạng. rtspsrc trả về dòng các gói tin RTP này (application/x-rtp)
+# rtph264depay: trích xuất phần H.264 trong gói RTP, gom các mẩu H.264 bị băm lại thành 1 khung hình H.264 hoàn chỉnh, xuất ra luồng H.264 (thường ở dạng byte-stream).
+# ... phần còn lại tương tự như trong ghi chú task1.1/run_mp4_nv.sh.
